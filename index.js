368335442cbbcb61e522ff058b8e2fce1963512b
Nikita Yustovskiy <yustovskiy@ya.ru>
2017-12-13 12:57:05 +0300

    Removed the line for name attr

--- index.js
+++ index.js
@@ -20,8 +20,6 @@
         location.hostname == this.hostname
       ) {
         var target = $(this.hash)
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-
         if (target.length) {
           event.preventDefault()
           var offset = isHeaderFixed ? settings.offset : headerHeight + settings.offset
